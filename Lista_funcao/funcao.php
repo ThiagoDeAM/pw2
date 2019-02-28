051f9eac33e9467782cae5f4c9d432eb1dadc461
Thiago <Thiago@Thiago-PC>
2019-02-28 20:36:07 -0300

    Lista 3 concluída!

--- Lista_funcao/funcao.php
+++ Lista_funcao/funcao.php
@@ -1,39 +1,82 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title></title>
+	<title>Lista 3</title>
 	<meta charset="utf-8">
 </head>
 <body>
-    <h1>First Answer</h1>
-<?php 
-    $numero = 13;
-    function primo($numero):bool{
-        
-        $primo=1;
-        $cont=0;
-        
-        while($primo<=$numero){
-            if($numero%$primo==0){
-            $cont=$cont+1;
-        }
-        $primo++;
-        }
+    
+    <?php 
+        echo "- - - - - Exercício 1 - - - - -<br><br>";
+        $numero = 17;
+        function primo($numero):bool{
 
-        if($cont==2){
-            return true;
+            $primo=1;
+            $cont=0;
+
+            while($primo<=$numero){
+                if($numero%$primo==0){
+                $cont++;
+            }
+            
+            $primo++;
+            
+            }
+
+            if($cont==2){
+                return true;
+            }else{
+                return false;
+            }
+        }
+        
+        if(primo($numero)){
+           echo $numero. " - Primo<br>";
         }else{
-            return false;
+            echo $numero. " - Composto<br>";
         }
-    }
-    if(primo($numero)){
-       echo "Primo";
-    }else{
-        echo "Composto";
-    }
 
+        echo "<br><br>- - - - - Exercício 2 - - - - -<br><br>";
 
+        for($i=1;$i<=100;$i++){
+            if(primo($i)){
+                print $i." ";
+            }
+        }
+
+        echo "<br><br>- - - - - Exercício 3 - - - - -<br><br>";   
     ?>
     
+    <ul>
+        <?php
+
+            $color=array(
+                'Preto',
+                'Branco',
+                'Verde',
+                'Vermelho',
+                'Marrom',
+                'Rosa',
+                'Azul',
+                'Roxo',
+                'Amarelo',
+                'Cinza'
+            );
+         
+            function lista_nao_ordenada_de_cores_HTML($color){
+                for($j=0;$j < count($color);$j++){    
+            ?>                          
+                <li> 
+                    <?php 
+                    echo $color[$j];               
+                    ?>
+                </li>
+        <?php  
+                 }
+            }
+            lista_nao_ordenada_de_cores_HTML($color);
+        ?>
+                
+    </ul>
 </body>
 </html>
