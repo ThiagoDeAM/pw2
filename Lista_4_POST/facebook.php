9c1230697e9ded790a208c12e384b264e818d58e
1831186 <1831186@LABAMAQ01.itp.local>
2019-03-12 13:45:31 -0300

    Lista 4 em desenvolvimento!

--- Lista_4_POST/facebook.php
+++ Lista_4_POST/facebook.php
@@ -8,16 +8,18 @@
     <h1>Abra uma conta</h1>
     <h3>É gratuito e sempre será.</h3>
 
-
 	    <input type="text" name="nome" placeholder="Nome">
-	    <input type="text" name="sobrenome" placeholder="Sobrenome">
-	    <input type="text" name="email" placeholder="E-mail ou número do celular">
-	    <input type="texto" name="email_conf" placeholder="Insira novamente o email ou o celular">
-	    <input type="password" name="senha" placeholder="Nova senha">    	
-
+	    <input type="text" name="sobrenome" placeholder="Sobrenome"><br>
+	    <inputtype="text" name="email" placeholder="E-mail ou número do celular"><br>
+	    <input type="texto" name="email_conf" placeholder="Insira novamente o email ou o celular"><br>
+	    <input type="password" name="senha" placeholder="Nova senha">  	
+   	
+<style type="text/css">
+	
+</style>
     <h2>Aniversário</h2>
-    <input type="radio" name="senha"><p>Feminino</p> 
-    <input type="radio" name="senha"><p>Masculino</p>
+
+	
 	<select>
 
 		<option>Dia</option>
@@ -57,15 +59,11 @@
 				10=>Nov,
 				11=>Dez,
 			);
-			for($i=0;$i<31;$i++){
-				$vet[$i]=$i+1;
-				$soma=$soma+$i;
-			}
-			for($i=0;$i<31;$i++){
+			for($j=0;$j<12;$j++){
 			?>	
 		<option>
 			<?php
-				echo $vet[$i];
+				echo $vet_1[$j];
 			?>
 
 		</option>
@@ -73,9 +71,32 @@
 			}
 		?>
 	</select>
+	<select>
 
+		<option>Ano</option>
+			<?php
+			$vet_2=array();
+			for($k=1904;$k<2019;$k++){
+				$vet_2[$k]=$k+1;
+				$soma=$soma+$k;
+			}
+			for($k=1904;$k<2019;$k++){
+			?>	
+		<option>
+			<?php
+				echo $vet_2[$k];
+			?>
 
-    <p>Ao clicar em Inscreva-se, você concorda com nossos Termos, Política de Dados e Política de Cookies. Você pode receber notificações por SMS e pode cancelar isso quando quiser.</p>
+		</option>
+		<?php 
+			}
+		?>
+	</select>
+	<a href="#">Por que preciso informar minha data de nascimento?</a><br>
+    <input type="radio" name="senha"><p>Feminino</p> 
+    <input type="radio" name="senha"><p>Masculino</p>
+
+    <a href="#"><p>Ao clicar em Inscreva-se, você concorda com nossos Termos, Política de Dados e Política de Cookies. Você pode receber notificações por SMS e pode cancelar isso quando quiser.</p></a>
     <button>Abrir uma conta</button>
 
 
