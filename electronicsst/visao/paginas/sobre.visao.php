25e5fc906c6213d220a1f6fb779d60ed49b90baf
1831186 <1831186@LABBMAQ17.itp.local>
2019-04-15 10:56:35 -0300

    Lista MVCd Concluída!

---
+++ electronicsst/visao/paginas/sobre.visao.php
@@ -0,0 +1,3 @@
+<h1>Sobre</h1>
+<h4>Nossa História</h4>
+<p>Electronic's ST é uma Loja de eletrônicos online que dita tendências ao oferecer produtos de primeira qualidade e atendimento ao consumidor no conforto de sua casa. Nosso negócio é focado na inovação e orientado a constantemente atualizar e melhorar a experiência de compras online.</p>
