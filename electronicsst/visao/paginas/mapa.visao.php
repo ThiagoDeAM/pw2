1508dbc2443bd9f733807188fa30eeadf897205e
Thiago <Thiago@Thiago-PC>
2019-04-18 20:13:58 -0300

    Pasta electronicsst excluída!

--- electronicsst/visao/paginas/mapa.visao.php
+++
@@ -1,2 +0,0 @@
-<h1>Mapa</h1>
-<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3656.3010408302225!2d-48.023378984406136!3d-23.593534168667677!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94c5cc0aa577fd79%3A0x94275cc265f55968!2sAv.+Jo%C3%A3o+Ol%C3%ADmpio+de+Oliveira+-+Vila+Asem%2C+Itapetininga+-+SP!5e0!3m2!1spt-BR!2sbr!4v1543014894277" width="100%" height="500" frameborder="0" style="border:0" allowfullscreen></iframe>
